202874d4afdce98df3479f0a2e3cd98cbb93c0ba
Iryna Arshulik <irynarshulik@gmail.com>
2026-06-15 10:19:28 +0300

    Implement order reservation and shipment flow

--- MaterialWarehouse.BLL/Interfaces/IOrderService.cs
+++ MaterialWarehouse.BLL/Interfaces/IOrderService.cs
@@ -5,6 +5,12 @@ namespace MaterialWarehouse.BLL.Interfaces;
 
 public interface IOrderService
 {
-    Task<Result<OrderDto>> CreateOrderAsync(int userId, IEnumerable<(int materialId, int quantity)> items);
-    Task<Result> TransitionOrderStateAsync(int orderId, string nextState);
+    Task<Result<OrderDto>> CreateOrderAsync(
+        int userId,
+        IEnumerable<(int materialId, int quantity)> items);
+
+    Task<Result> TransitionOrderStateAsync(
+        int orderId,
+        string nextState,
+        int managerId = 0);
 }
